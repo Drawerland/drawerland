fb64f2ba7e69418d4084647020a19ec9e9931268
Ay00One <Xzefiri@live.fr>
2017-11-17 20:37:50 +0100

    Purification of `Keyboard.eventExecuter()`

--- virtual/Keyboard.js
+++ virtual/Keyboard.js
@@ -7,54 +7,33 @@ var drawerland = drawerland || {};
         this.game = game;
         const self = this;
         this.eventExecuter = function(event){
-            array = self.game.map.getAdjacentBoxes(self.game.map.getGridBox(self.game.character.position.gridX,self.game.character.position.gridY));
-            if(event.keyCode == 49) {//#1
-                try{
-                    if(!array[0].blocking && !array.selected){
+
+            function boxSelect(index){
+                var array = self.game.map.getAdjacentBoxes(self.game.map.getGridBox(self.game.character.position.gridX,self.game.character.position.gridY));
+                if (index < array.length){
+                    if(!array[index].blocking && ~array.selected){
                         for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[0].select();
+                        array[index].select();
                     }
-                }catch(e){console.error(e)}
+                }
+            }
+            if(event.keyCode == 49) {//#1
+                boxSelect(0);
             }
             else if(event.keyCode == 50) {//#2
-                try{
-                    if(!array[1].blocking && !array.selected){
-                        for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[1].select();
-                    }
-                }catch(e){console.error(e)}
+                boxSelect(1);
             }
             else if(event.keyCode == 51) {//#3
-                try{
-                    if(!array[2].blocking && !array.selected){
-                        for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[2].select();
-                    }
-                }catch(e){console.error(e)}
+                boxSelect(2);
             }
             else if(event.keyCode == 52) {//#4
-                try{
-                    if(!array[3].blocking && !array.selected){
-                        for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[3].select();
-                    }
-                }catch(e){console.error(e)}
+                boxSelect(3);
             }
             else if(event.keyCode == 53) {//#5
-                try{
-                    if(!array[4].blocking && !array.selected){
-                        for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[4].select();
-                    }
-                }catch(e){console.error(e)}
+                boxSelect(4);
             }
             else if(event.keyCode == 54) {//#6
-                try{
-                    if(!array[5].blocking && !array.selected){
-                        for (var i = 0; i < array.length; i++) {array[i].unselect();}
-                        array[5].select();
-                    }
-                }catch(e){console.error(e)}
+                boxSelect(5);
             }
             self.game.update();
         }
